1ff799c6f88412b640821e3dfdde63b232b3472c
dommiloo <156959975+dommiloo@users.noreply.github.com>
2024-03-16 01:12:12 +0000

    1st commit

--- main.hpp
+++ main.hpp
@@ -1,8 +1,36 @@
 
-//********************
-// Declare Function Prototypes
-//********************
+#include <iostream>
+using namespace std;
 
-// ******************************
-// Implement all your functions here
-// ******************************
+bool isPrime(int num) {
+    if (num <= 1)
+        return false;
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0)
+            return false
+    }
+    return true;
+}
+
+void getTwoValues(int& begin, int& end) {
+    do {
+        cout << "Enter two integer values (begin and end): ";
+        cin >> begin >> end;
+    } while (begin >= end);
+}
+
+int getNextPrime(int begin) {
+    int nextPime = begin + 1;
+    while (!isPrime(nextPrime)) {
+        nextPrime++;
+    }
+    return nextPrime;
+}
+
+int getPrevPrime(int end) {
+    int prevPrime = end - 1;
+    while (!isPrime(prevPrime)) {
+        prevPrime--;
+    
+    return prevPrime
+}
